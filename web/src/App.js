3f866f9a0f06758569f2aef2f4f1d2dd57b20696
Lee Chi Ho <u3578552@connect.hku.hk>
2022-10-05 00:09:57 +0800

    fix: git typo

---
+++ web/src/App.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react'
+
+function App() {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+  
+  return (
+    <div>
+      {typeof data.members}
+      {
+        data.members.forEach((item) => {
+          return (
+            <div>
+              <h1>{item}</h1>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ web/src/login.js
@@ -0,0 +1,8 @@
+
+function login() {
+    return (   
+        <>
+            Login To The System
+        </> 
+    )
+}
