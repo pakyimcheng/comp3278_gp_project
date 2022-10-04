43809400201b5aa0e5728b15b397ea133c23a7eb
Lee Chi Ho <u3578552@connect.hku.hk>
2022-10-05 00:08:32 +0800

    create flask and react apps, change readme to add flask command

---
+++ README.md
@@ -0,0 +1,9 @@
+### Start Flask Server
+#### Windows
+
+    1. .\venv\Scripts\Activate
+    2. py server.py
+    
+#### MacOS/Linux
+    1. source venv/bin/activate
+    2. python server.py
---
+++ flask/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# /test API Router
+@app.route("/test")
+def members():
+    return {"test": ["1", "2", "3"]}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
