b45da43cfb5d445914b3d6c8e38ba759f216ca94
Lee Chi Ho <u3578552@connect.hku.hk>
2022-10-19 17:28:34 +0800

    added one example sql query "/student/<studentID>"

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,60 @@
+import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from classifier import get_prediction
 import json
 
+from dotenv import load_dotenv
+
+import mysql.connector
+from mysql.connector import errorcode
+
 DEV_SERVER_URL = "http://127.0.0.1:3000/"
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+try:
+    cnx = mysql.connector.connect(
+        user="root",
+        password=os.getenv("MYSQL_PASSWORD"),
+        host="localhost",
+        database="project",
+    )
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+
+print(cnx)
+cursor = cnx.cursor()
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/student/<studentID>", methods=["POST"])
+def student(studentID):
+    cursor.execute(
+        "SELECT studentID, name, email_address FROM student WHERE studentID = %s",
+        (studentID,),
+    )
+    s = cursor.fetchone()
+    result = {
+        "studentID": s[0],
+        "name": s[1],
+        "email_address": s[2],
+    }
+    print(result)
+    return result
 
 
 @app.route("/test")
