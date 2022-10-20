86f7152fb115ef2b38b07e7bdd3fc233f4176d26
Lee Chi Ho <u3578552@connect.hku.hk>
2022-10-20 15:36:07 +0800

    feat: add main and record pages

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,13 +1,113 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import "./home.css";
 
-function Home() {
-
+function home({ ...props }) {
 	return (
-		<>
-			<h1>this is the homepage</h1>
-		</>
+		<div className="landing--main-">
+			<div className="frame-20">
+				<div className="frame-4">
+					<div className="frame-6">
+						<div className="screencap-2022-10-13-at-23-19-1"></div>
+					</div>
+					<div className="frame-8">
+						<div className="welcome-back-">Welcome back!</div>
+						<div className="mark">Mark</div>
+					</div>
+					<div className="frame-7">
+						<div className="login-records">Login Records</div>
+					</div>
+					<div className="frame-10">
+						<div className="frame-202">
+							<svg
+								className="bell-1"
+								width="32"
+								height="33"
+								viewBox="0 0 32 33"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M28 25.8334V27.1667H4V25.8334L6.66667 23.1667V15.1667C6.66667 11.0334 9.37333 7.39341 13.3333 6.22008C13.3333 6.08675 13.3333 5.96675 13.3333 5.83341C13.3333 5.12617 13.6143 4.44789 14.1144 3.9478C14.6145 3.4477 15.2928 3.16675 16 3.16675C16.7072 3.16675 17.3855 3.4477 17.8856 3.9478C18.3857 4.44789 18.6667 5.12617 18.6667 5.83341C18.6667 5.96675 18.6667 6.08675 18.6667 6.22008C22.6267 7.39341 25.3333 11.0334 25.3333 15.1667V23.1667L28 25.8334ZM18.6667 28.5001C18.6667 29.2073 18.3857 29.8856 17.8856 30.3857C17.3855 30.8858 16.7072 31.1667 16 31.1667C15.2928 31.1667 14.6145 30.8858 14.1144 30.3857C13.6143 29.8856 13.3333 29.2073 13.3333 28.5001"
+									fill="black"
+								/>
+							</svg>
+							<div className="notification">Notification</div>
+						</div>
+						<div className="-notif-date---notif-----">
+							&lt;Notif date&gt;
+							<br />
+							&lt;Notif&gt;
+							<br />
+							...
+						</div>
+					</div>
+					<div className="frame-11">
+						<div className="frame-203">
+							<svg
+								className="clock-time-five-1"
+								width="32"
+								height="32"
+								viewBox="0 0 32 32"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M16 2.66663C8.66667 2.66663 2.66667 8.66663 2.66667 16C2.66667 23.3333 8.66667 29.3333 16 29.3333C23.3333 29.3333 29.3333 23.3333 29.3333 16C29.3333 8.66663 23.3333 2.66663 16 2.66663ZM18.6667 22.6666L14.6667 15.7333V9.33329H16.6667V15.2L20.4 21.7333L18.6667 22.6666Z"
+									fill="black"
+								/>
+							</svg>
+							<div className="_00-00">00:00</div>
+						</div>
+						<div className="login-time--69-420">Login time: 69:420</div>
+					</div>
+				</div>
+				<div className="frame-204">
+					<div className="frame-62">
+						<div className="welcome-to-icms">WELCOME to ICMS</div>
+						<div className="welcome-to-our-system--mark--this-system-provide-the-best-experience-in-organising-and-monitoring-your-courses--you-can-select-the-features-below-">
+							Welcome to our system, Mark!
+							<br />
+							This system provide the best experience in organising and
+							monitoring your courses. You can select the features below.
+						</div>
+					</div>
+					<div className="frame-5">
+						<div className="frame-72">
+							<svg
+								className="timetable-2"
+								width="64"
+								height="64"
+								viewBox="0 0 64 64"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M37.3333 31.9999H41.3333V39.5199L47.84 43.2799L45.84 46.7466L37.3333 41.8399V31.9999ZM10.6667 5.33325H48C49.4145 5.33325 50.771 5.89515 51.7712 6.89535C52.7714 7.89554 53.3333 9.2521 53.3333 10.6666V26.9333C56.64 30.2933 58.6667 34.9066 58.6667 39.9999C58.6667 44.9506 56.7 49.6986 53.1993 53.1992C49.6987 56.6999 44.9507 58.6666 40 58.6666C34.9067 58.6666 30.2933 56.6399 26.9333 53.3332H10.6667C9.25219 53.3332 7.89563 52.7713 6.89544 51.7712C5.89525 50.771 5.33334 49.4144 5.33334 47.9999V10.6666C5.33334 9.2521 5.89525 7.89554 6.89544 6.89535C7.89563 5.89515 9.25219 5.33325 10.6667 5.33325ZM10.6667 39.9999V47.9999H23.12C21.9733 45.5732 21.3333 42.8532 21.3333 39.9999H10.6667ZM10.6667 21.3333H26.6667V13.3333H10.6667V21.3333ZM48 21.3333V13.3333H32V21.3333H48ZM10.6667 34.6666H22.1067C23.0133 31.5999 24.6933 28.8533 26.9333 26.6666H10.6667V34.6666ZM40 27.0666C36.5699 27.0666 33.2802 28.4292 30.8548 30.8547C28.4293 33.2801 27.0667 36.5698 27.0667 39.9999C27.0667 47.1466 32.8533 52.9333 40 52.9333C41.6984 52.9333 43.3802 52.5987 44.9494 51.9488C46.5185 51.2988 47.9443 50.3461 49.1453 49.1452C50.3462 47.9442 51.2989 46.5184 51.9488 44.9493C52.5988 43.3801 52.9333 41.6983 52.9333 39.9999C52.9333 32.8533 47.1467 27.0666 40 27.0666Z"
+									fill="black"
+								/>
+							</svg>
+							<div className="timetable">Timetable</div>
+						</div>
+						<div className="frame-82">
+							<svg
+								className="exclamation-thick-1"
+								width="64"
+								height="64"
+								viewBox="0 0 64 64"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M26.6667 8H37.3333V37.3333H26.6667V8ZM26.6667 56V45.3333H37.3333V56H26.6667Z"
+									fill="black"
+								/>
+							</svg>
+							<div className="upcoming-courses">Upcoming courses</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
-export default Home;
+export default home;
--- web/src/pages/records.js
+++ web/src/pages/records.js
@@ -1,13 +1,151 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import "./records.css";
 
-function Records() {
-
+function records({ ...props }) {
 	return (
-		<>
-			<h1>this is the records page</h1>
-		</>
+		<div className="check-login-record">
+			<div className="frame-20">
+				<div className="frame-4">
+					<div className="frame-6">
+						<div className="screencap-2022-10-13-at-23-19-1"></div>
+					</div>
+					<div className="frame-8">
+						<div className="user-name">User Name</div>
+						<div className="mark">Mark</div>
+					</div>
+					<div className="frame-40">
+						<div className="current-ip">Current IP</div>
+						<div className="xxx-xxx-xxx-xxx">XXX.XXX.XXX.XXX</div>
+					</div>
+					<div className="frame-41">
+						<div className="current-os">Current OS</div>
+						<div className="mac-win">Mac/Win</div>
+					</div>
+					<div className="frame-42">
+						<div className="current-browser">Current Browser</div>
+						<div className="chrome-xx-xx">Chrome XX.XX</div>
+					</div>
+				</div>
+				<div className="frame-202">
+					<div className="frame-34">
+						<svg
+							className="history-1"
+							width="87"
+							height="86"
+							viewBox="0 0 87 86"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M48.8749 28.6667H43.4999V46.5833L58.8366 55.685L61.4166 51.3492L48.8749 43.8958V28.6667ZM47.0833 10.75C38.53 10.75 30.3271 14.1478 24.2791 20.1958C18.231 26.2439 14.8333 34.4468 14.8333 43H4.08325L18.2733 57.4408L32.7499 43H21.9999C21.9999 36.3475 24.6426 29.9674 29.3467 25.2634C34.0507 20.5594 40.4307 17.9167 47.0833 17.9167C53.7358 17.9167 60.1158 20.5594 64.8198 25.2634C69.5239 29.9674 72.1666 36.3475 72.1666 43C72.1666 49.6525 69.5239 56.0326 64.8198 60.7366C60.1158 65.4406 53.7358 68.0833 47.0833 68.0833C40.1674 68.0833 33.8966 65.2525 29.3816 60.7017L24.2933 65.79C30.1341 71.6667 38.1249 75.25 47.0833 75.25C55.6365 75.25 63.8394 71.8522 69.8874 65.8042C75.9355 59.7561 79.3333 51.5532 79.3333 43C79.3333 34.4468 75.9355 26.2439 69.8874 20.1958C63.8394 14.1478 55.6365 10.75 47.0833 10.75Z"
+								fill="white"
+							/>
+						</svg>
+						<div className="all-login-records">All Login Records</div>
+					</div>
+					<div className="frame-9">
+						<div className="frame-25">
+							<div className="no-">No.</div>
+						</div>
+						<div className="frame-22">
+							<div className="time">Time</div>
+						</div>
+						<div className="frame-23">
+							<div className="login-duration">Login Duration</div>
+						</div>
+						<div className="frame-24">
+							<div className="ip-address">IP Address</div>
+						</div>
+					</div>
+					<div className="frame-5">
+						<div className="frame-35">
+							<div className="frame-222">
+								<div className="_1-">1.</div>
+							</div>
+							<div className="frame-252">
+								<div className="-time-">&lt;time&gt;</div>
+							</div>
+							<div className="frame-232">
+								<div className="-stayed-for-">&lt;stayed for&gt;</div>
+							</div>
+							<div className="frame-242">
+								<div className="-ip-">&lt;IP&gt;</div>
+							</div>
+						</div>
+						<div className="frame-36">
+							<div className="frame-222">
+								<div className="_2-">2.</div>
+							</div>
+							<div className="frame-252">
+								<div className="-time-">&lt;time&gt;</div>
+							</div>
+							<div className="frame-232">
+								<div className="-stayed-for-">&lt;stayed for&gt;</div>
+							</div>
+							<div className="frame-242">
+								<div className="-ip-">&lt;IP&gt;</div>
+							</div>
+						</div>
+						<div className="frame-37">
+							<div className="frame-222">
+								<div className="_3-">3.</div>
+							</div>
+							<div className="frame-252">
+								<div className="-time-">&lt;time&gt;</div>
+							</div>
+							<div className="frame-232">
+								<div className="-stayed-for-">&lt;stayed for&gt;</div>
+							</div>
+							<div className="frame-242">
+								<div className="-ip-">&lt;IP&gt;</div>
+							</div>
+						</div>
+						<div className="frame-38">
+							<div className="frame-222">
+								<div className="_4-">4.</div>
+							</div>
+							<div className="frame-252">
+								<div className="-time-">&lt;time&gt;</div>
+							</div>
+							<div className="frame-232">
+								<div className="-stayed-for-">&lt;stayed for&gt;</div>
+							</div>
+							<div className="frame-242">
+								<div className="-ip-">&lt;IP&gt;</div>
+							</div>
+						</div>
+						<div className="frame-39">
+							<div className="frame-222">
+								<div className="_5-">5.</div>
+							</div>
+							<div className="frame-252">
+								<div className="-time-">&lt;time&gt;</div>
+							</div>
+							<div className="frame-232">
+								<div className="-stayed-for-">&lt;stayed for&gt;</div>
+							</div>
+							<div className="frame-242">
+								<div className="-ip-">&lt;IP&gt;</div>
+							</div>
+						</div>
+						<div className="frame-402">
+							<div className="frame-222">
+								<div className="_6-">6.</div>
+							</div>
+							<div className="frame-252">
+								<div className="-time-">&lt;time&gt;</div>
+							</div>
+							<div className="frame-232">
+								<div className="-stayed-for-">&lt;stayed for&gt;</div>
+							</div>
+							<div className="frame-242">
+								<div className="-ip-">&lt;IP&gt;</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
-export default Records;
+export default records;
