0b5375797924b3bb35ab3ece985a836461c17f84
pakyimcheng <78531639+pakyimcheng@users.noreply.github.com>
2022-11-08 10:31:10 +0800

    feat: install timetable pkg

--- web/src/pages/timetable.js
+++ web/src/pages/timetable.js
@@ -1,6 +1,24 @@
-import React from "react";
+import * as React from 'react'
+import Timetable from 'react-timetable-events'
 
 function TimeTable() {
+	const [timetable, setTimetable] =  useState();
+
+	// useEffect(() => {
+	// 	async function a() {
+	// 		await axios
+	// 			.post("http://127.0.0.1:5001/getWeeklyTimetable?studentID=" + studentID)
+	// 			.then(async function (res) {
+	// 				setCourseInfo(res.data);
+	// 				setCourseID(res.data.courseID);
+	// 			})
+	// 			.catch((err) => {
+	// 				console.log(err);
+	// 			});
+	// 	}
+	// 	a();
+	// }, []);
+
 	return (
 		<>
 			<h1>this is the timetable</h1>;
